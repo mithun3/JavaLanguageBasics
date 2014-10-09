e400b338ffdc27b3e8c45624b4b2dd99c47f6908
Mithun <mithun3@gmail.com>
2014-10-09 15:54:29 +0530

    added hascode

---
+++ src/_18_hashcode/StringHashCode.java
@@ -0,0 +1,9 @@
+package _18_hashcode;
+
+public class StringHashCode {
+
+  public static void main(String[] args) {
+    System.out.println("Hello".hashCode());
+    System.out.println("Hello".hashCode());
+  }
+} ///:~
